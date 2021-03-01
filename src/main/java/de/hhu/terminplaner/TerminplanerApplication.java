7d5d6a967f2220936d6464b67e94e0caee8c6feb
aghiadaboud <aghiad.aboud@gmail.com>
2021-03-01 18:43:24 +0100

    initial commit

---
+++ src/main/java/de/hhu/terminplaner/TerminplanerApplication.java
@@ -0,0 +1,13 @@
+package de.hhu.terminplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminplanerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TerminplanerApplication.class, args);
+  }
+
+}
