3eb43bc6a4bab2e967f061c89fdecb0cacb6cb7c
aghiadaboud <aghiad.aboud@gmail.com>
2021-03-06 00:52:26 +0100

    last commit for today

--- src/main/java/de/hhu/terminplaner/controller/MainController.java
+++ src/main/java/de/hhu/terminplaner/controller/MainController.java
@@ -1,4 +1,4 @@
-package de.hhu.terminplaner.web;
+package de.hhu.terminplaner.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/de/hhu/terminplaner/controller/OrganisatorController.java
+++ src/main/java/de/hhu/terminplaner/controller/OrganisatorController.java
@@ -1,17 +1,9 @@
-package de.hhu.terminplaner.web;
+package de.hhu.terminplaner.controller;
 
 
-import de.hhu.terminplaner.model.Tag;
-import de.hhu.terminplaner.model.Tutor;
-import de.hhu.terminplaner.model.Zeitslot;
-import java.sql.Date;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,20 +14,11 @@ public class OrganisatorController {
 
   @GetMapping("/setup")
   public String setup(Model model) {
-    //model.addAttribute("zeitslot", new Zeitslot("", List.of(new Tutor(), new Tutor())));
-    model.addAttribute("tag",
-        new Tag("", List.of(
-            new Zeitslot("z1", List.of(new Tutor(), new Tutor(), new Tutor(), new Tutor())))));
     return "organisator/setup";
   }
 
   @PostMapping("/setup")
-  public String configure(@ModelAttribute("tag") Tag tag) {
-    LocalDate localDate = LocalDate.parse(tag.getDatum());
-    System.out.println(new SimpleDateFormat("EEEE").format(Date.valueOf(localDate)));
-    System.out.println(tag.getZeitslots());
-    System.out.println(tag.getDatum());
-    System.out.println("////////////////////////////////");
+  public String configure() {
     return "organisator/setup";
   }
 }
