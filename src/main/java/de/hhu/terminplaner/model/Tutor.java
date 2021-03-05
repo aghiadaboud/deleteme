a5a2f4d56c735ecc7d24d7b84432f7e183dfe213
aghiadaboud <aghiad.aboud@gmail.com>
2021-03-06 00:43:15 +0100

    deleted model classes

--- src/main/java/de/hhu/terminplaner/model/Tutor.java
+++ src/main/java/de/hhu/terminplaner/model/Tutor.java
@@ -1,13 +1,21 @@
 package de.hhu.terminplaner.model;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Tutor {
 
-  private String name;
+    @Id
+    private Long id;
+
+    private String githubname;
+    private Zeitslot zeitslot;
+
+
 }
--- src/main/java/de/hhu/terminplaner/model/Zeitslot.java
+++ src/main/java/de/hhu/terminplaner/model/Zeitslot.java
@@ -1,16 +1,25 @@
 package de.hhu.terminplaner.model;
 
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Zeitslot {
 
-  private String uhrzeit;
-  private List<Tutor> tutoren = new ArrayList<>();
+
+    @Id
+    private Long id;
+    //private LocalDate datum;
+    private String wochentag;
+    private String uhr;
+    private Gruppe gruppe;
+
+    public Zeitslot(String wochentag, String uhr) {
+        this.wochentag = wochentag;
+        this.uhr = uhr;
+    }
 }
